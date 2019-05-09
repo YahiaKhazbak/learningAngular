d74fac3188ea6b3769d4a05d412019a37c799f23
DominikLipinski <maboonga@gmail.com>
2019-05-09 20:44:23 +0200

    recipe model

--- recipes-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'Test description here', 
+    "https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg"),
+    new Recipe('A test Recipe 2 ', 'Another Test description', 
+    "https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg")
+  ];
 
   constructor() { }
 
