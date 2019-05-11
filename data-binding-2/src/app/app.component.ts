3c39add2a48143712ce14e9826c81feb45cf7150
DominikLipinski <maboonga@gmail.com>
2019-05-11 18:14:34 +0200

    binding to custom events

---
+++ data-binding-2/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+//import { Server } from 'http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type: 'server', name: 'testserver', content: 'test content'}];
+
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+}
