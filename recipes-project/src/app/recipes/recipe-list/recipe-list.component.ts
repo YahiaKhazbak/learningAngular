f8b4cc7c251de92661332579585be699053c53b5
DominikLipinski <maboonga@gmail.com>
2019-05-19 13:18:33 +0200

    @Input and @Output communication with grand-parent

--- recipes-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
     new Recipe('A test Recipe', 'Test description here', 
     "https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg"),
@@ -16,8 +17,13 @@ export class RecipeListComponent implements OnInit {
   ];
 
   constructor() { }
-
   ngOnInit() {
   }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+    console.log(`data in recipe list ${recipe}`);
+  }
+
 
 }
